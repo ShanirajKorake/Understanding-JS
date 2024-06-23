1571faf0dd4715d5cc3aea48b7f3ce15a98f97bb
om <korkeom999@gmail.com>
2024-06-23 17:05:28 +0530

    Initial commit

---
+++ 01_Base/01_variables.js
@@ -0,0 +1,14 @@
+const accId = 144552
+let accEmail = "om@gmail.com"
+var accPass = "1221"
+accCity = "Pune"
+
+
+// accId = 1221
+
+accEmail = "om1@gmail.com"
+accPass = "21212121"
+accCity = "Mumabi"
+
+console.table([accId,accEmail, accPass, accCity])
+
