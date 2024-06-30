791e7ac4cddf3da3de89f326c2fdeeafd0044d00
om <korkeom11@gmail.com>
2024-06-30 23:53:16 +0530

    nums and maths done

---
+++ 01_Base/06_nums_n_maths.js
@@ -0,0 +1,37 @@
+const score = 299
+// console.log(score);
+
+const balance = new Number(199)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+
+const num = 1221.1242
+// console.log(num.toPrecision(5));
+
+const amount = 11221112
+// console.log(amount.toLocaleString('en-IN'));
+
+
+
+
+
+
+//maths
+
+// console.log(Math);
+// console.log(Math.abs(-12));
+// console.log(Math.round(1.2));
+// console.log(Math.ceil(22.1));
+// console.log(Math.floor(22.1));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
