d81846dd534a436b603a136b1417a1e8cf1d3faa
om <korkeom11@gmail.com>
2024-06-30 15:22:57 +0530

    Day 3 initial comit

--- 01_Base/05_strings.js
+++ 01_Base/05_strings.js
@@ -3,4 +3,26 @@ const repoCount = 7
 
 // console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('om')
+const gameName = new String('omkorke')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(7));
+// console.log(gameName.indexOf('e'));
+
+// const newName = gameName.substring(0,3)
+// console.log(newName);
+
+
+const str1 = gameName.slice(0,10);
+console.log(str1);
+
+
+const str2 = "    om    "
+
+console.log(str2);
+console.log(str2.trim());
