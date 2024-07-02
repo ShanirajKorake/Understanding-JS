ac4ab596c0235ed791c0d29d2b32be5beea5a15d
om <korkeom11@gmail.com>
2024-07-02 15:09:08 +0530

    IIFE done.

---
+++ 03_Base/04_iife.js
@@ -0,0 +1,11 @@
+// Imediately Invoked Function Expression [IIFE]
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('shani')
