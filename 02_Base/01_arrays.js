4eb28b21e0191749263a1b901e686cf85c57a3eb
om <korkeom11@gmail.com>
2024-07-02 09:12:49 +0530

    day 5 initial commit

---
+++ 02_Base/01_arrays.js
@@ -0,0 +1,15 @@
+const arr1 = [1, 2, 3, 4, 5]
+const heros = ["flash",  "batman", "superman"]
+const arr2 = new Array(1, 2, 3, 4, 5)
+
+console.log(heros[1]);
+console.log(arr2);
+
+//array methods 
+
+// arr2.push(6)
+// arr2.unshift(12)
+// arr2.shift()
+
+console.log(arr2.includes(2))
+console.log(arr2);
