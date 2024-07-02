3e093a7f4829ed116ada710ef3f6bae52dc81931
om <korkeom11@gmail.com>
2024-07-02 11:30:34 +0530

    arrays done

--- 02_Base/01_arrays.js
+++ 02_Base/01_arrays.js
@@ -1,15 +1,39 @@
-const arr1 = [1, 2, 3, 4, 5]
-const heros = ["flash",  "batman", "superman"]
-const arr2 = new Array(1, 2, 3, 4, 5)
+// array
 
-console.log(heros[1]);
-console.log(arr2);
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
 
-//array methods 
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
 
-// arr2.push(6)
-// arr2.unshift(12)
-// arr2.shift()
+// Array methods
 
-console.log(arr2.includes(2))
-console.log(arr2);
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
