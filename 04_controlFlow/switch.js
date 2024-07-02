b10501b5f9079e022afe09ea10715297986a91f8
om <korkeom11@gmail.com>
2024-07-02 18:43:31 +0530

    control flow statements done.

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    default:
+        console.log("defaulte case");
+        break;
+}
